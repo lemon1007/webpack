21fbc975689bf3ae4434e05e11042645d409fff9
sherry <lemon19961007@sina.com>
2022-08-13 11:44:51 +0800

    test npm run start

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,13 @@ var path = require('path');
 
 module.exports = {
     mode: 'development',
+    devtool: "inline-source-map",
+    devServer:{
+        static:'./dist'
+    },
     entry: './src/index.js',
     output: {
-        filename: 'index.[contenthash].js'
+        filename: 'index.[hash].js'
     },
     plugins: [
         new HtmlWebpackPlugin({
