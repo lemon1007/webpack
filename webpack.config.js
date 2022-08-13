c4d1a4dc7c8f9ec6424904ac153f8e056edfd047
sherry <lemon19961007@sina.com>
2022-08-13 12:38:46 +0800

    两个config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,36 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
     mode: 'development',
     devtool: "inline-source-map",
-    devServer:{
-        static:'./dist'
+    devServer: {
+        static: './dist',
+        port: 107
     },
     entry: './src/index.js',
     output: {
-        filename: 'index.[hash].js'
+        // name可以自行指定，指定格式为 index.[hash].js，不指定则默认为main
+        filename: '[name].[hash].js'
     },
     plugins: [
         new HtmlWebpackPlugin({
             title: '首页',
-            template:'assets/model.html'
-        })
+            template: 'assets/model.html'
+        }),
+        new MiniCssExtractPlugin({
+            // name可以自行指定，指定格式为 index.css，不指定则默认为main
+            filename: '[name].[hash].css',
+            chunkFilename: '[id].[hash].css',
+            ignoreOrder: false,
+        }),
     ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
         ],
     },
